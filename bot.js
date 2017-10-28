d0502e6b3fba3cebe5108037e834c884a5bc1509
MichaelEpicA <33169033+MichaelEpicA@users.noreply.github.com>
2017-10-28 13:04:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,34 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
-const prefix = "="
+const bot = new Discord.Client();
+const prefix = "&"
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+var help = new Discord.RichEmbed()
+  .addField("There are the commands" , "&ping checks bots connection".true)
+  .addField("&eval Only for owner".true)
+  .setColor(0x0000FF)
+bot.login(process.env.BOT_TOKEN)
+
+bot.on('ready', function() {
+  console.log(`Ready as: ${bot.user.tag}`)
 })
-client.on("message", function(msg){
+bot.on("message", function(msg){
   console.log(`${msg.author}in #${msg.channel.name} or ${msg.guild.name} ${msg.content}`)
 })
 
-client.on('message', msg => {
+bot.on("message",function(msg) {
   if(msg.author.bot || !msg.content.startsWith(prefix)) {
     return;
   }
   var command = msg.content.split(" ")[0].slice(prefix.length).toLowerCase()
   var args = msg.content.split(" ").slice(1);
-  let say = args.join(" ")
+  let suffix = args.join(" ")
   if(command === "ping") {
 	msg.channel.send("Pinging...").then(function(m) {
     m.edit(`Pong! Took ${m.createdTimestamp - msg.createdTimestamp}ms`)
 	})
   }
   var help = new Discord.RichEmbed()
-  .addField("Help", "Thanks for jmmousee for beeing a good sport anyways the commands are &help, &ping and &eval")
+  .addField("Help", "Thanks for jmmousee for beeing a good sport anyways the commands are &help, &ping, &eval and &kick")
   .setColor(0x0000FF)
   if(command === "help") {
 msg.channel.sendEmbed(help);
@@ -30,7 +36,7 @@ msg.channel.sendEmbed(help);
 if(command === "kick") {
   const reason = args.slice(1).join(' ');
   const user = msg.mentions.users.first();
-  const modlog = client.channels.find('name', 'mod-log');
+  const modlog = bot.channels.find('name', 'mod-log');
   if (!modlog) return msg.reply('I cannot find a mod-log channel');
   if (reason.length < 1) return msg.reply('You must supply a reason for the kick.');
   if (msg.mentions.users.size < 1) return msg.reply('You must mention someone to kick them.').catch(console.error);
@@ -42,10 +48,10 @@ if(command === "kick") {
     .setColor(0x00AE86)
     .setTimestamp()
     .setDescription(`**Action:** Kick\n**Target:** ${user.tag}\n**Moderator:** ${msg.author.tag}\n**Reason:** ${reason}`);
-  return client.channels.get(modlog.id).send({embed});
+  return bot.channels.get(modlog.id).send({embed});
 }
 })
-
+ 
 function clean(text) {
   if (typeof(text) === "string")
     return text.replace(/`/g, "'" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
@@ -53,11 +59,11 @@ function clean(text) {
       return text;
 }
 
-client.on("message", message => {
+bot.on("message", message => {
   const args = message.content.split(" ").slice(1);
 
 if (message.content.startsWith(prefix + "eval")) {
-    if(message.author.id !== "239847961717112833") return;
+    if(message.author.id !== "162587551721324544") return;
     try {
       var code = args.join(" ");
       var evaled = eval(code);
@@ -66,7 +72,7 @@ if (message.content.startsWith(prefix + "eval")) {
         evaled = require("util").inspect(evaled);
 
         var embed = new Discord.RichEmbed()
-  .addField("The code is down there v" , clean(evaled), {code:"xl"}.true)
+  .addField("Eval" , clean(evaled), {code:"xl"}.true)
   .setColor(0x0000FF)
 message.channel.sendEmbed(embed);
   } catch (err) {
@@ -77,5 +83,3 @@ message.channel.sendEmbed(error);
   }
  }
 });
-
-client.login(process.env.BOT_TOKEN);
