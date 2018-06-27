52ab7a68c058e86a177cbad19ffeffd55dce2f43
MichaelEpicA <33169033+MichaelEpicA@users.noreply.github.com>
2018-06-27 13:10:47 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,91 +1,68 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
-const fs = require("fs");
-const botSettings = require("./botsettings.json")
-const prefix = botSettings.prefix
-bot.commands = new Discord.Collection();
+const prefix = "="
 
-fs.readdir("./Commands/", (err,files) => {
+bot.on('ready', () => {
+  bot.user.setStatus('dnd', 'dnd');
+	
+ bot.user.setPresence({ game: { name: 'Fucking my dick', type: 0 } });
+});
 
-if(err) console.log(err);
 
-var jsfile = files.filter(f => f.split(".").pop() === "js")
-if(jsfile.length <= 0){
-	console.log("oof no commands")
-	return;
-}
-jsfile.forEach((f,i) =>{
-var props = require(`./Commands/${f}`);
-console.log(`${f} loaded`)
-bot.commands.set(props.help.name, props)
-})
-})
-
-//const db = require('quick.db');
-//var db = require('quick.db')
-
-bot.login(botSettings.token)
-//bot.login(process.env.BOT_TOKEN)
-
+var help = new Discord.RichEmbed()
+  .addField("There are the commands" , "&ping checks bots connection".true)
+  .addField("&eval Only for owner".true)
+  .setColor(0x0000FF)
+bot.login(process.env.BOT_TOKEN)
 
 bot.on('ready', function() {
   console.log(`Ready as: ${bot.user.tag}`)
-  console.log(`In ${bot.guilds.size} servers`)
-  console.log(`https://discordapp.com/oauth2/authorize?client_id=336874091057381386&scope=bot&permissions=1610083455`)
-	  bot.user.setActivity(`in ${bot.guilds.size} servers| &help`, { type: "Listening" });
 })
-
+bot.on("message", function(msg){
+  console.log(`${msg.author} ${msg.content}`)
+})
 
 bot.on("message",function(msg) {
-	if (!msg.content.startsWith(prefix))
- return;
-  //var command = msg.content.split(" ")[0].slice(prefix.length).toLowerCase()
-	var messageArray = msg.content.split(" ");
-  var cmd = messageArray[0];
-  var args = messageArray.slice(1);
-	var commandfile = bot.commands.get(cmd.slice(prefix.length));
-	  if(commandfile) commandfile.run(bot,msg,args);
+  if(msg.author.bot || !msg.content.startsWith(prefix)) {
+    return;
+  }
+  var command = msg.content.split(" ")[0].slice(prefix.length).toLowerCase()
+  var args = msg.content.split(" ").slice(1);
   let suffix = args.join(" ")
-	//if(msg.author.bot) return;
-	if(msg.channel.type === "dm") return;
+  if(command === "ping") {
+	msg.channel.send("Pinging...").then(function(m) {
+    m.edit(`Pong! Took ${m.createdTimestamp - msg.createdTimestamp}ms`)
+	})
+  }
+	if(command === "say") {
+		var msgg = args.join(" ");
+	msg.channel.send(msgg)
+	}
+  var help = new Discord.RichEmbed()
+  .addField("Help", "Thanks for jmmousee for beeing a good sport anyways the commands are &help, &ping, &eval and &kick")
+  .setColor(0x0000FF)
+  if(command === "help") {
+msg.channel.sendEmbed(help);
+  }
+if(command === "kick") {
+  const reason = args.slice(1).join(' ');
+  const user = msg.mentions.users.first();
+  const modlog = bot.channels.find('name', 'mod-log');
+  if (!modlog) return msg.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return msg.reply('You must supply a reason for the kick.');
+  if (msg.mentions.users.size < 1) return msg.reply('You must mention someone to kick them.').catch(console.error);
 
+  if (!msg.guild.member(user).kickable) return msg.reply('I cannot kick that member');
+  msg.guild.member(user).kick();
 
- // var noreason = new Discord.RichEmbed()
-  //.addField("Kick/Ban", "Please Supply a reason")
- // .setFooter("Mouse-Bot#9560", bot.user.avatarURL)
-  //.setColor(0x0000FF)
-
-// var notnsfw = new Discord.RichEmbed()
-// .addField("NSFW", "This channel is not NSFW")
-//  .setFooter("Mouse-Bot#9560", bot.user.avatarURL)
-//  .setColor(0x0000FF)
-//f(command === "lewd") {
-//	msg.delete();
- // if(!msg.channel.nsfw) return msg.channel.sendEmbed(notnsfw);
-//  var nsfw = ['https://cdn.discordapp.com/attachments/422584638154997761/422914876768518154/lewd_neko373.jpeg', 'https://cdn.discordapp.com/attachments/422584638154997761/422914857529245718/lewd_neko807.jpg', 'https://cdn.discordapp.com/attachments/422584638154997761/422914839032233984/lewd_neko490.jpeg', 'https://cdn.discordapp.com/attachments/422584638154997761/422914802524880896/lewd_neko399.jpeg', 'https://cdn.discordapp.com/attachments/422584638154997761/422914800532717571/lewd_neko215.jpeg', 'https://cdn.discordapp.com/attachments/422584638154997761/422914224025632769/lewd_neko296.jpeg', 'https://cdn.discordapp.com/attachments/422584638154997761/422913841345855489/lewd_neko107.jpeg', 'https://cdn.discordapp.com/attachments/422584638154997761/422913774291517440/lewd_neko363.jpg', 'https://cdn.discordapp.com/attachments/422584638154997761/422913738694459395/lewd_neko162.png', 'https://cdn.discordapp.com/attachments/422584638154997761/422913717894643712/lewd_neko057.jpg', 'https://cdn.discordapp.com/attachments/422584638154997761/423299470361624606/lewd_neko181.jpg']
-
-//  var lewd = Math.floor((Math.random() * nsfw.length));
-
-//  var isnsfw = new Discord.RichEmbed()
-//  .setTitle("NSFW")
- // .setImage(nsfw[lewd])
- // .setFooter("Mouse-Bot#9560", bot.user.avatarURL)
- // .setColor(0x0000FF)
- // msg.channel.sendEmbed(isnsfw)
-//}
-
-
-
-if(msg.content.includes("kek")) {
-    msg.channel.send("***Oh we kekkin?***", {files: ["./images/LMAO.png"]});
-		return;
-}
-if(msg.content.includes("gay")) {
-    msg.channel.send("***I am not gay........................................***", {files: ["./images/gay.jpg"]});
-		return;
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setDescription(`**Action:** Kick\n**Target:** ${user.tag}\n**Moderator:** ${msg.author.tag}\n**Reason:** ${reason}`);
+  return bot.channels.get(modlog.id).send({embed});
 }
 })
-
+ 
 function clean(text) {
   if (typeof(text) === "string")
     return text.replace(/`/g, "'" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
@@ -94,14 +71,10 @@ function clean(text) {
 }
 
 bot.on("message", message => {
-	var noot4u = new Discord.RichEmbed()
-  .addField("Eval", "This is not for you because you can destroy my whole bot and break and copy my code so that is why u no has access")
-  .setFooter("Mouse-Bot#9560", bot.user.avatarURL)
-  .setColor(0x0000FF)
   const args = message.content.split(" ").slice(1);
 
 if (message.content.startsWith(prefix + "eval")) {
-    if(message.author.id !== "162587551721324544") return message.channel.sendEmbed(noot4u);
+    if(message.author.id !== "342347518488608768") return;
     try {
       var code = args.join(" ");
       var evaled = eval(code);
@@ -110,16 +83,39 @@ if (message.content.startsWith(prefix + "eval")) {
         evaled = require("util").inspect(evaled);
 
         var embed = new Discord.RichEmbed()
-  .addField("Eval" , clean(evaled), {code:"xl"}.true)
-.setFooter("Mouse-Bot#9560", bot.user.avatarURL)
+  .addField("Evaled Code" , clean(evaled), {code:"xl"}.true)
+  .setColor(0x0000FF)
+message.channel.sendEmbed(embed);
+  } catch (err) {
+  var error = new Discord.RichEmbed()
+  .addField("rip the bot has been killed #fatalerror" , `\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``.true)
+  .setColor(0x0000FF)
+message.channel.sendEmbed(error);
+  }
+ }
+});
+
+bot.on("message", message => {
+  const args = message.content.split(" ").slice(1);
+
+if (message.content.startsWith(prefix + "eval")) {
+    if(message.author.id !== "162587551721324544") return;
+    try {
+      var code = args.join(" ");
+      var evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+        var embed = new Discord.RichEmbed()
+  .addField("There is the evaled code" , clean(evaled), {code:"xl"}.true)
   .setColor(0x0000FF)
 message.channel.sendEmbed(embed);
   } catch (err) {
   var error = new Discord.RichEmbed()
   .addField("rip there is a error" , `\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``.true)
-  .setFooter("Mouse-Bot#9560", bot.user.avatarURL)
   .setColor(0x0000FF)
 message.channel.sendEmbed(error);
   }
-}
-})
+ }
+});
